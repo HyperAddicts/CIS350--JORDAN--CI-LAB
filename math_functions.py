c47b39e44a9576e619dcb34b9f0d4d15d9267f7a
HyperAddicts <143756589+HyperAddicts@users.noreply.github.com>
2023-12-06 11:32:20 -0500

    Update math_functions.py

--- math_functions.py
+++ math_functions.py
@@ -13,8 +13,16 @@ def divide_numbers(a, b):
     else:
         return 'Error: Division by zero is not allowed.'
 
+def power_number(a, b):
+    return a ** b
+
+def sqrt_number(a):
+    return a ** 0.5
+
 if __name__ == "__main__":
     print("Adding:", add_numbers(2,4))
     print("Subtracting:", subtract_numbers(9,2))
     print("Multiplying:", multiply_numbers(2,4))
     print("Dividing:", divide_numbers(10,2))
+    print("Power:", power_number(2,3))
+    print("Square Root:", sqrt_number(9))
